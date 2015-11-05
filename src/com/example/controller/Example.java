35a8d7888a104129bb9cfd3af3426a5419a52ccf
dongjun <250191950@qq.com>
2015-11-05 15:28:29 +0800

    切换到idea工作环境
    Signed-off-by: dongjun <250191950@qq.com>

---
+++ src/com/example/controller/Example.java
@@ -0,0 +1,208 @@
+package com.example.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.common.base.BaseQueryRecords;
+import com.common.base.BaseResult;
+import com.common.base.BaseResultOK;
+import com.common.framework.FileDownload;
+import com.common.framework.FileUpload;
+import com.common.utils.LogUtils;
+import com.example.dao.Room;
+import com.example.dao.Student;
+import com.example.model.JSONModel;
+import com.example.model.POJOModel;
+import com.example.service.ExampleService;
+
+@Controller
+@RequestMapping("/example")
+// 统一的namespace， 可不定义
+public class Example {
+	Logger logger = LogUtils.getLogger(Example.class);
+
+	public Example() {
+		System.out.println("Example inited!!!");
+		
+	}
+	
+	/**
+	 * 跳转到页面视图，页面最终由 springmvc.xml中配置的prefix+返回值+suffix组成返回
+	 * 接口名为/example/jsp/noinput，其中/example来自类上的RequestMapping注解
+	 */
+	@RequestMapping(value = "/jsp/noinput")
+	public String tojsp() {
+		logger.debug("tojsp!!");
+		return "page/example/tojsp"; // 返回的的视图会由springmvc-servlet中的视图解析器解析
+	}
+
+	/**
+	 * 通过Model将数据输入到前台,前台可使用el表达式进行输出
+	 */
+	@RequestMapping(value = "/jsp/output")
+	public String tojspoutput(Model model) {
+		List<String> ss = new ArrayList<>();
+		ss.add("a");
+		ss.add("b");
+		ss.add("c");
+		ss.add("d");
+
+		model.addAttribute("ss", ss);
+		model.addAttribute("name", "test");
+		model.addAttribute("age", 28);
+		model.addAttribute("ss", "****@email.com");// key为value对应的类型名，这里为String
+		return "page/example/tojspoutput"; // 返回的的视图会由springmvc-servlet中的视图解析器解析
+	}
+
+	/**
+	 * 通过request来进行前后台数据传递
+	 */
+	@RequestMapping(value = "/jsp/reqinput")
+	public String reqinputoutput(HttpServletRequest request, Model model) {
+		model.addAttribute("name", request.getParameter("name"));
+		model.addAttribute("age", request.getParameter("age"));
+		model.addAttribute("string", request.getParameter("name2"));
+		model.addAttribute("name2", request.getParameter("name2"));
+		return "page/example/tojspoutput";
+	}
+
+	/**
+	 * 简单从前台获取参数, 注，如果Integer改为int，则前台必须传值，否则会报错，如果为Integer，前台不传值时，接收值为null
+	 */
+	@RequestMapping(value = "/jsp/simpleinput")
+	public String simpleinput(String name2, Integer age, String name,
+			Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		model.addAttribute("name2", name2);
+		return "page/example/tojspoutput";
+	}
+
+	/**
+	 * 通过模型POJO从前台获取参数,前台直接使用modelinput的属性名进行传值，若为list，可通过[0],[1]...的方式进行赋值
+	 */
+	@RequestMapping(value = "/jsp/pojoinput")
+	public String pojoinput(POJOModel modelinput, Model model) {
+		modelinput.setString("xxxxx");
+		model.addAttribute(modelinput);
+		return "page/example/tojspoutput2";
+	}
+
+	/**
+	 * 返回json视图,返回数据为具有get/set的数据模型， 添加注解ResponseBody
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/json/output")
+	public JSONModel tojsonoutput() {
+		JSONModel jsonModel = new JSONModel();
+		List<String> list = new ArrayList<>();
+		Map<String, Object> map = new HashMap<>();
+
+		jsonModel.setDouble1(121212.12);
+		jsonModel.setDoublt1(121212);
+		jsonModel.setFloat1((float) 12.12);
+		jsonModel.setString("helloworl11d111");
+
+		list.add("11");
+		list.add("12");
+		list.add("13");
+		list.add("14");
+		jsonModel.setList(list);
+
+		map.put("a", "aa");
+		map.put("b", "bb");
+		map.put("c", "cc");
+		map.put("d", null);
+		jsonModel.setMap(map);
+		return jsonModel;
+	}
+
+	/**
+	 * 文件下载
+	 * 
+	 * @return
+	 * @return
+	 */
+	@RequestMapping("/download")
+	public void download(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		String path = "E:\\cn_windows_7_ultimate_with_sp1_x64_dvd_618537.iso";
+		FileDownload.download(path, request, response);
+	}
+
+	/**
+	 * 文件上传
+	 */
+	@RequestMapping("/upload")
+	@ResponseBody
+	public BaseResult upload(HttpServletRequest request,
+			HttpServletResponse response) throws IllegalStateException,
+			IOException {
+		FileUpload.upload(request, response, "d:/");
+		return new BaseResultOK();
+	}
+
+	public void doexception() throws Exception {
+		@SuppressWarnings("unused")
+		int s = 10 / 0;
+	}
+
+	/**
+	 * 异常捕获
+	 */
+	@RequestMapping("/exception")
+	@ResponseBody
+	public BaseResult exception(Boolean on) throws Exception {
+		if (on) {
+			doexception();
+		}
+		return new BaseResultOK();
+	}
+
+	@Resource(name = "exampleService")
+	private ExampleService exampleService;
+
+	/**
+	 * 查看所有学生
+	 */
+	@RequestMapping("/queryallstu")
+	@ResponseBody
+	public BaseQueryRecords<Student> queryAllStu(HttpServletRequest request,
+			HttpServletResponse response, Model model) {
+		BaseQueryRecords<Student> stu = this.exampleService.queryAllStudent();
+		return stu;
+	}
+
+	/**
+	 * 查看所有学生
+	 */
+	@RequestMapping("/queryallstujson")
+	@ResponseBody
+	public Student queryAllStu() {
+		Student stu = this.exampleService.queryStu(2);
+		return stu;
+	}
+
+	/**
+	 * 查看班级
+	 */
+	@RequestMapping("/queryallroomjson")
+	@ResponseBody
+	public BaseQueryRecords<Room> queryAllRoom() {
+		BaseQueryRecords<Room> rm = this.exampleService.queryRoom(2);
+		return rm;
+	}
+}
---
+++ src/com/example/dao/ExampleDao.java
@@ -0,0 +1,40 @@
+package com.example.dao;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.sql.JoinType;
+import org.springframework.stereotype.Repository;
+
+import com.common.base.BaseQueryRecords;
+import com.common.basedao.impl.BaseDaoDB;
+
+@Repository("exampleDao")
+public class ExampleDao extends BaseDaoDB {
+
+    public ExampleDao() {
+        System.out.println("ExampleDao inited!!");
+    }
+
+    public BaseQueryRecords<Student> queryAllStu() {
+        @SuppressWarnings("unchecked")
+        BaseQueryRecords<Student> student = (BaseQueryRecords<Student>) super
+                .find(super.getCriteria(Student.class));
+
+        return student;
+    }
+
+    public Student queryStu(int id) {
+        return (Student) super
+                .find(super.getCriteria(Student.class)
+                        .add(Restrictions.eq("id", id)).createCriteria("room"))
+                .getData().get(0);
+    }
+
+    @SuppressWarnings("unchecked")
+    public BaseQueryRecords<Room> queryRoom(int id) {
+        return (BaseQueryRecords<Room>) super.find(super
+                .getCriteria(Room.class)
+                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
+                .createCriteria("students", JoinType.LEFT_OUTER_JOIN));
+    }
+}
