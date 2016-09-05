380a94fcbccc8b376a5f47e31fc10b20da8eeefe
zhuangjiabin <willgrey@vip.qq.com>
2016-09-05 17:25:27 +0800

    修改IExampleDao Signed-off-by: zhuangjiabin <willgrey@vip.qq.com>

--- project/com/example/dao/IExampleDao.java
+++ project/com/example/dao/IExampleDao.java
@@ -1,13 +1,14 @@
 package com.example.dao;
 
 import com.common.base.BaseRecords;
+import com.common.base.dao.BaseDao;
 import com.example.dao.model.Room;
 import com.example.dao.model.Student;
 
 /**
  * Created by Will on 2016/9/5 13:58.
  */
-public interface IExampleDao {
+public interface IExampleDao extends BaseDao{
     BaseRecords<Student> queryAllStu();
 
     BaseRecords<Student> queryAllStu(int page, int rows);
